cec7d78f8d9d7a9f830a6553b5e36ff26ea11150
Noo00bdev <koffiyvann.200@gmail.com>
2025-10-06 15:41:19 +0000

    finishing a commit message.

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-
+import { afficherTache, ChangeTheme , sauvegarderTaches } from "./function.js"
 
 const listItem = document.querySelector('#listItems')
 const btn = document.querySelector('#btnAdd')
+const checked = document.querySelector('#checked')
+const Nocheck = document.querySelector('#no-checked')
+const Delete = document.querySelector('#del')
+const delAll = document.querySelector('#delAll')
+const history = document.querySelector('#history')
+const body = document.body
+const title = document.querySelector('#title')
+const theme = document.querySelector('#themeToggle')
 
+// store les listes sous formes json dans le localStorage
+let tasks = JSON.parse(localStorage.getItem('list')) || [];
 
 btn.addEventListener('click', function(e){
-
     e.preventDefault()
     const form = document.querySelector('#todo')
     listItem.classList.toggle('listItem')
@@ -14,70 +23,26 @@ btn.addEventListener('click', function(e){
     const taskInput = document.querySelector('#taskInput')
     const taskValue = taskInput.value
     if(!taskValue) return
-    afficherTache(taskValue)
+    afficherTache(taskValue, tasks)
 
     // met la taches dans tasks
-    tasks.push({text: `${taskValue}`} )
+    tasks.push({text: `${taskValue}`, done : false})
     // Sauvegarde dans le localSt"Nouvelle tâche dans localStorage
     localStorage.setItem('list', JSON.stringify(tasks));
+
 
     form.reset()
 })
 
-// affiche les taches
-function afficherTache(taskValue) {
-    const taskDiv = document.createElement('div');
-    taskDiv.className = "list flex flex-row gap-5 items-center border p-2 m-2 rounded";
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = "w-5 h-5 cursor-pointer";
-
-    const taskContent = document.createElement('p');
-    taskContent.innerText = taskValue;
-    taskContent.className = "font-bold";
-
-    taskContent.setAttribute("contenteditable", "true")
-
-    taskContent.addEventListener("input", sauvegarderTaches)
-
-    
-
-    // Le button supprimer
-    const deleteBtn = document.createElement('button')
-    deleteBtn.innerHTML = `
-        <img src="delete.png">
-    `
-    deleteBtn.addEventListener('click', ()=>{
-        taskDiv.remove()
-    })
-    deleteBtn.className ="absolute right-3 bg-red-900 p-1 rounded-xs"
-
-    // div.append(checkbox, taskContent)
-    taskDiv.append(checkbox, taskContent , deleteBtn);
-    listItem.append(taskDiv);
-}
-
-
-function sauvegarderTaches() {
-  // On sélectionne toutes les tâches dans la liste
-  const elements = listItem.querySelectorAll('.list');
-
-  // On recrée le tableau des tâches
-  tasks = Array.from(elements).map(item => {
-    const text = item.querySelector('p').textContent.trim(); // le texte de la tâche
-    const done = item.querySelector('input[type="checkbox"]').checked; // état de la case
-    return { text, done };
-  });
-
-  // On sauvegarde le tableau dans le localStorage
-  localStorage.setItem('list', JSON.stringify(tasks));
-}
+// change le theme 
+theme.addEventListener('click', (e) => {
+    ChangeTheme(e, body)
+    title.classList.toggle('text-white')
+    history.classList.toggle('text-white')
+})
 
 
 
-const checked = document.querySelector('#checked')
-const Nocheck = document.querySelector('#no-checked')
 
 checked.addEventListener('click', function(){
 
@@ -95,7 +60,6 @@ checked.addEventListener('click', function(){
 
 
 Nocheck.addEventListener('click', function(){
-
     Nocheck.classList.toggle('selected')
 
     if(checked.classList.contains('selected')){
@@ -113,7 +77,7 @@ Nocheck.addEventListener('click', function(){
 
 
 
-const Delete = document.querySelector('#del')
+
 
 // Supprime les taches cocher
 Delete.addEventListener('click', function(e){
@@ -129,7 +93,9 @@ Delete.addEventListener('click', function(e){
     })
 })
 
-const delAll = document.querySelector('#delAll')
+
+
+// button pour tout suprimer sur la page
 delAll.addEventListener('click', function(e){
     e.preventDefault()
     //selectionner toute les task
@@ -140,16 +106,12 @@ delAll.addEventListener('click', function(e){
     })
 })
 
-const history = document.querySelector('#history')
-// store les listes sous formes json dans le localStorage
-let tasks = JSON.parse(localStorage.getItem('list')) || [];
+
 
 // Afficher les tâches existantes
-
-
 tasks.forEach(task => {
     const taskDiv = document.createElement('div');
-    taskDiv.className = "list flex gap-5 items-center bg-blue-600/30 p-2 mt-2 rounded";
+    taskDiv.className = " flex gap-5 items-center bg-blue-600/30 p-2 mt-2 rounded";
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
@@ -163,3 +125,5 @@ tasks.forEach(task => {
     taskDiv.append(checkbox, taskContent);
     history.append(taskDiv);
 });
+
+
