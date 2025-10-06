718d8d24cf0ffc5fd28198475946eb4e49367691
Noo00bdev <koffiyvann.200@gmail.com>
2025-10-06 15:39:01 +0000

    amelioration du bouton supprimer amelioration du code

---
+++ function.js
@@ -0,0 +1,64 @@
+
+const listItem = document.querySelector('#listItems')
+export function sauvegarderTaches(listItem, tasks) {
+  // On sélectionne toutes les tâches dans la liste
+  const elements = listItem.querySelectorAll('.list');
+
+
+  // On recrée le tableau des tâches
+  tasks = Array.from(elements).map(item => {
+    const text = item.querySelector('p').textContent.trim(); // le texte de la tâche
+    const done = item.querySelector('input[type="checkbox"]').checked; // état de la case
+    return { text, done };
+  });
+
+  // On sauvegarde le tableau dans le localStorage
+  localStorage.setItem('list', JSON.stringify(tasks));
+}
+/**
+ * 
+ * @param {String} Value 
+ */
+export function afficherTache(Value, tasks) {
+    const taskDiv = document.createElement('div');
+    taskDiv.className = "list flex flex-row gap-5 items-center border p-2 m-2 rounded";
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = "w-5 h-5 cursor-pointer";
+
+    const taskContent = document.createElement('p');
+    taskContent.innerText = Value;
+    taskContent.className = "font-bold";
+
+    taskContent.setAttribute("contenteditable", "true")
+    taskContent.addEventListener("input", ()=>sauvegarderTaches(listItem, tasks))
+    checkbox.addEventListener("change",()=>sauvegarderTaches(listItem, tasks) )
+    taskContent.addEventListener('focus', () => {
+        taskContent.className = "outline-none";
+        taskDiv.classList.toggle("border-blue-600");
+    });
+
+
+    // Le button supprimer
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerHTML = `
+        <img src="delete.png">
+    `
+    deleteBtn.addEventListener('click', ()=>{
+        taskDiv.remove()
+    })
+    deleteBtn.className ="absolute right-3 bg-red-900 p-1 rounded-xs"
+
+    // div.append(checkbox, taskContent)
+    taskDiv.append(checkbox, taskContent , deleteBtn);
+    listItem.append(taskDiv);
+}
+
+
+
+export function ChangeTheme(e, value){
+    e.preventDefault()
+    value.classList.toggle('bg-gray-900/80')
+
+}
